0490f528499ea2519712879c0086a37ba79b9e53
cynomain <68111337+cynomain@users.noreply.github.com>
2023-05-11 18:32:23 +0700

    functionality

--- script.js
+++ script.js
@@ -18,39 +18,146 @@ var msgbox_buttons = document.getElementById("messagebox_buttons");
 </div>
 </div>*/
 
-var answers = []; //[{0, 1}]
-var currentNumber = null;
+const DEFAULT_ANSWER_VALUE = ["", -1]; //None and uncorrected
+const LS_CURRENTLY_OPENED = "currentlyOpened";
+const LS_CURRENT_NUMBER = "currentNumber";
+const SYMBOL_CHECKMARK = "✔";
+const EMOJI_CHECKMARK = "✅";
+const SYMBOL_X = "🗙";
+const EMOJI_X = "❌";
+
+class AnswerSheet {
+  //name = "placeholder";
+  answers = ["", -1];
+  getLength() {
+    return this.answers.length;
+  }
+}
+
+var currentOpen = new AnswerSheet();
+var currentNumber = 1;
 
 window.onload = () => {
-  //load answers
-  if (sessionStorage.getItem("hasLoaded") === "true") {
-    answers = JSON.parse(sessionStorage["answers"]);
-    currentNumber = Number(sessionStorage["currentNumber"]);
-  }
+  LoadProgress();
 };
 
 window.onbeforeunload = () => {
   SaveProgress();
 };
 
+function LoadProgress() {
+  if (localStorage.getItem(LS_CURRENTLY_OPENED) != null) {
+    currentOpen = JSON.parse(localStorage[LS_CURRENTLY_OPENED]);
+    currentNumber = Number(localStorage[LS_CURRENT_NUMBER]);
+    console.log("Loaded!");
+    console.log(currentOpen);
+    console.log(currentNumber);
+  } else {
+    console.log("no saves");
+  }
+}
+
 function SaveProgress() {
-  if (answers != null && currentNumber != null) {
-    sessionStorage["hasLoaded"] = "true";
-    sessionStorage["answers"] = JSON.stringify(answers);
-    sessionStorage["currentNumber"] = currentNumber;
+  localStorage[LS_CURRENTLY_OPENED] = JSON.stringify(currentOpen);
+  localStorage[LS_CURRENT_NUMBER] = currentNumber;
+}
+
+function ResetProgress() {
+  currentOpen = new AnswerSheet();
+  console.log(currentOpen);
+  currentNumber = 0;
+  SaveProgress();
+  updateDisplay();
+}
+
+function DownloadSave_JSON() {
+  let json = JSON.stringify(currentOpen);
+  download("answer_sheet", json);
+}
+
+function DownloadSave_Readable(useEmojis) {
+  let readable = GetReadable(useEmojis);
+  download("answer_sheet", readable);
+}
+
+function GetReadable(useEmojis) {
+  let text = "";
+  let marks = useEmojis
+    ? [EMOJI_X, EMOJI_CHECKMARK]
+    : [SYMBOL_X, SYMBOL_CHECKMARK];
+  for (let index = 0; index < currentOpen.answers.length; index++) {
+    const element = currentOpen.answers[index];
+    let mark = element[1] === -1 ? "" : marks[element[1]];
+    text += `${number + 1}. ${element[0]} ${mark}\n`;
+  }
+  return text;
+}
+
+function download(filename, text) {
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+/*
+function SaveOverwriteSheet(answerSheet){
+
+  localStorage.setItem("save_" + answerSheet.name);
+}
+
+function AddFileName()
+
+function getDefaultName(){
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key.startsWith("save_")) {
+      const value = localStorage.getItem(key);
+      
+    }
   }
 }
 
+function getAllLocalStorage(){
+  return { ...localStorage };
+}
+*/
 
-function setAnswer(num, ans) {
-  if (answers.length < num) {
-    answers.fill(["", -1], answers.length - 1, num);
+function setAnswer(number, answer) {
+  if (currentOpen.length <= number) {
+    currentOpen.answers.fill(
+      currentOpen.answers.length - 1,
+      number - 1,
+      DEFAULT_ANSWER_VALUE
+    );
+    currentOpen.answers.push([answer, -1]);
+  } else {
+    currentOpen.answers[number][0] = answer;
   }
-  answers[num] = [ans, -1];
+  console.log("Set answer for number " + (number + 1) + " to " + answer);
 }
 
-function setCorrect(num, isCorrect) {
-  answers[num][1] = isCorrect ? 0 : 1;
+function setCorrect(number, correct) {
+  if (this.answers.length > number) {
+    let a;
+    if (typeof correct === "number") {
+      a = correct;
+    } else {
+      a ? "1" : "0";
+    }
+    this.answers[number][1] = a;
+    console.log("Set correctness for number " + (number + 1) + " to " + a);
+  } else {
+    console.log("ERROR: ans.len < num");
+  }
 }
 
 function clamp(value, min, max) {
--- view/view.js
+++ view/view.js
@@ -1,34 +1,46 @@
-//Debug
-window.onload = () => {
-    var entryElement = document.createElement("div");
-    entryElement.className = "showEntry";
-    var p1 = document.createElement("p");
-    p1.innerText = "1.";
-    p1.className = "number"
-    var p2 = document.createElement("p");
-    p2.innerText = "ANS";
-    p2.className = "answer";
-    var p3 = document.createElement("p");
-    p3.innerText = "✔";
-    p3.className = "correct";
-    var image = document.createElement("button");
-    var icon = document.createElement("img");
-    icon.src = "assets/icon_edit.svg" ;
-    icon.style = "height:100%; width: auto;";
-    image.className = "imageButton edit";
-    image.id = "button_edit";
-    image.style = "color:white;";
-    image.appendChild(icon);
-    entryElement.appendChild(p1);
-    entryElement.appendChild(p2);
-    entryElement.appendChild(p3);
-    entryElement.appendChild(image);
-  
-    var parent = document.getElementById("entries");
-  
-    for (let i = 0; i < 40; i++) {
-      parent.appendChild(entryElement.cloneNode(true));
-    }
-  
-  
-  };
+const parent = document.getElementById("entries");
+
+addEventListener("load", (e) => {Load();});
+
+function Load() {
+  for (let index = 0; index < currentOpen.answers.length; index++) {
+    const element = currentOpen.answers[index];
+    createEntry(index, element[0], element[1]);
+  }
+  console.log("View loaded");
+}
+
+function createEntry(number, answer, correctness) {
+  const correctsymbols = ["", SYMBOL_X, SYMBOL_CHECKMARK];
+  let entryElement = document.createElement("div");
+  entryElement.className = "showEntry";
+  let p1 = document.createElement("p");
+  p1.innerText = number + 1 + ". ";
+  p1.className = "number";
+  let p2 = document.createElement("p");
+  p2.innerText = answer;
+  p2.className = "answer";
+  let p3 = document.createElement("p");
+  p3.innerText = correctsymbols[correctness + 1];
+  p3.className = "correct";
+  let image = document.createElement("button");
+  let icon = document.createElement("img");
+  icon.src = "/assets/icon_edit.svg";
+  icon.style = "height:100%; width: auto;";
+  image.className = "imageButton edit";
+  image.id = "button_edit";
+  image.style = "color:white;border-radius: 0px; padding:0;";
+  image.appendChild(icon);
+  entryElement.appendChild(p1);
+  entryElement.appendChild(p2);
+  entryElement.appendChild(p3);
+  entryElement.appendChild(image);
+  parent.appendChild(entryElement);
+  return entryElement;
+}
+
+function createTest() {
+  for (let i = 0; i < 40; i++) {
+    createEntry(0, "A", 0);
+  }
+}
