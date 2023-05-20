2b0d4618802555f523088d6b08ea4b4fd084c5ac
cynomain <68111337+cynomain@users.noreply.github.com>
2023-05-20 10:32:59 +0700

    rework

--- scripts/pilgan/mainscript.js
+++ scripts/pilgan/mainscript.js
@@ -34,12 +34,12 @@ const EMOJI_CHECKMARK = "✅";
 const SYMBOL_X = "X";
 const EMOJI_X = "❌";
 
-var sound_prev = new Audio("/assets/aud_prev.ogg");
-var sound_next = new Audio("/assets/aud_next.ogg");
-var sound_reset = new Audio("/assets/aud_reset.ogg");
-var sound_msgbox = new Audio("/assets/aud_msgbox.ogg");
-var sound_choose = new Audio("/assets/aud_choose.ogg");
-var sound_score = new Audio("/assets/aud_score.ogg");
+var sound_prev = new Audio("/assets/pilgan/aud_prev.ogg");
+var sound_next = new Audio("/assets/pilgan/aud_next.ogg");
+var sound_reset = new Audio("/assets/pilgan/aud_reset.ogg");
+var sound_msgbox = new Audio("/assets/pilgan/aud_msgbox.ogg");
+var sound_choose = new Audio("/assets/pilgan/aud_choose.ogg");
+var sound_score = new Audio("/assets/pilgan/aud_score.ogg");
 
 class AnswerSheet {
   //name = "placeholder";
@@ -59,19 +59,43 @@ window.onload = () => {
   ui_settings = document.getElementById("settingsUI");
   ui_view = document.getElementById("showUI");
   transitionBlack = document.getElementById("transitionBlack");
-
-  sound_prev = new Audio("/assets/aud_prev.ogg");
-  sound_next = new Audio("/assets/aud_next.ogg");
-  sound_reset = new Audio("/assets/aud_reset.ogg");
-  sound_msgbox = new Audio("/assets/aud_msgbox.ogg");
-  sound_choose = new Audio("/assets/aud_choose.ogg");
-  sound_score = new Audio("/assets/aud_score.ogg");
   LoadProgress();
 };
 
 window.onbeforeunload = () => {
   SaveProgress();
 };
+
+var isFullScreen = false;
+
+function ToggleFullscreen() {
+  const elem = document.documentElement;
+
+  if (isFullScreen) {
+    //IS FULLSCREEN
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      /* IE11 */
+      document.msExitFullscreen();
+    }
+    isFullScreen = false;
+  } else {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+      /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      /* IE11 */
+      elem.msRequestFullscreen();
+    }
+    isFullScreen = true;
+  }
+}
 
 function LoadProgress() {
   if (localStorage.getItem(LS_ANSWERS) != null) {
@@ -269,29 +293,29 @@ function back() {
 function gotoHome() {
   //document.location.href = "/index.html";
   //doTransition(() => {
-    setEnabled(ui_main);
-    setDisabled(ui_settings);
-    setDisabled(ui_view);
-    updateDisplay();
+  setEnabled(ui_main);
+  setDisabled(ui_settings);
+  setDisabled(ui_view);
+  updateDisplay();
   //});
 }
 
 function gotoView() {
   //document.location.href = "/pages/view.html";
   //doTransition(() => {
-    setDisabled(ui_main);
-    setDisabled(ui_settings);
-    setEnabled(ui_view);
-    ViewLoad();
+  setDisabled(ui_main);
+  setDisabled(ui_settings);
+  setEnabled(ui_view);
+  ViewLoad();
   //});
 }
 
 function gotoSettings() {
   //document.location.href = "/pages/settings.html";
   //doTransition(() => {
-    setDisabled(ui_main);
-    setEnabled(ui_settings);
-    setDisabled(ui_view);
+  setDisabled(ui_main);
+  setEnabled(ui_settings);
+  setDisabled(ui_view);
   //});
 }
 
